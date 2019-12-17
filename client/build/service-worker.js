965a9954d0d709eac2812368534dbbf3131c40bc
Kevin Le <kevin_le@sfu.ca>
2019-12-17 03:51:50 -0800

    add buildpack

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/client/build/precache-manifest.b75a1dafa7cbe6c13ca73ab772e7129f.js"
+  "/cradle-vsa.github.io/precache-manifest.0a6e8b3f48bbe0c874dc6c029d9f24b0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/client/build/index.html", {
+workbox.routing.registerNavigationRoute("/cradle-vsa.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
--- client/src/serverUrl.js
+++ client/src/serverUrl.js
@@ -3,7 +3,7 @@ console.log("process.env.NODE_ENV", process.env.NODE_ENV);
 let BASE_URL;
 
 if(process.env.NODE_ENV == "production") {
-    BASE_URL = "https://cmpt373.csil.sfu.ca:8048/api"
+    BASE_URL = "https://api-cradle.herokuapp.com/api"
 } else {
     BASE_URL = "http://localhost:5000/api"
 }
