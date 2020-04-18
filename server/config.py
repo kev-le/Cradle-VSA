9b6ec708569e139af0cd7b391dc9de4e818f8f81
Kevin Le <kevin_le@sfu.ca>
2020-04-17 18:13:31 -0700

    Add postgresql database

--- server/config.py
+++ server/config.py
@@ -19,6 +19,7 @@ class Config(object):
     env.read_env()
 
     try: 
+
         db_user = env("DB_USERNAME")
         db_pw = env("DB_PASSWORD")
     except environs.EnvError:
@@ -26,7 +27,7 @@ class Config(object):
         print("DB_USERNAME or DB_PASSWORD environment variable not set")
         print("*******************************************************")
 
-    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://b28b636b8ab912:1273ab9c@us-cdbr-iron-east-05.cleardb.net/heroku_8638673d0ddea21' # ex: 'mysql+pymysql://root:123456@localhost:3306/mydb'
+    SQLALCHEMY_DATABASE_URI = env("DATABASE_URL") # ex: 'mysql+pymysql://root:123456@localhost:3306/mydb'
     SQLALCHEMY_POOL_RECYCLE = 120
     SQLALCHEMY_POOL_TIMEOUT = 20
 
