ad9326845aec993f83aa7cf83a3120932677a3a9
Kevin Le <kevin_le@sfu.ca>
2020-05-06 17:26:17 -0700

    Serve react app from server

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cradle-vsa.github.io/precache-manifest.0a6e8b3f48bbe0c874dc6c029d9f24b0.js"
+  "./precache-manifest.bc9472f8d35cd2d46b7b3b2b8d567712.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/cradle-vsa.github.io/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
--- client/src/serverUrl.js
+++ client/src/serverUrl.js
@@ -1,14 +1,14 @@
 console.log("process.env.NODE_ENV", process.env.NODE_ENV);
 
 let BASE_URL;
-let PUBLIC_URL;
-if(process.env.NODE_ENV == "production") {
-    BASE_URL = "https://api-cradle.herokuapp.com/api"
-    PUBLIC_URL = "https://cradle-app.herokuapp.com/"
+
+if (process.env.BASE_URL !== undefined && process.env.BASE_URL !== null) {
+    BASE_URL = process.env.BASE_URL;
+}
+else if(process.env.NODE_ENV == "production") {
+    BASE_URL = "https://cradle-app.herokuapp.com/api"
 } else {
     BASE_URL = "http://localhost:5000/api"
-    PUBLIC_URL = "http://localhost:3000";
 }
 
-export  { PUBLIC_URL };
 export default BASE_URL;
